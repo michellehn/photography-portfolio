121ee124a7360d3dff61ae73ae3f6f8163337993
Michelle Nelson <michellehnelson@Michelle-Nelsons-MacBook-Pro.local>
2014-12-29 03:06:45 -0800

    Updated after moving index

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8"/>
+    <title>Michelle Nelson Photography</title>
+    <link rel="stylesheet" type="text/css" href="style/mystyle.css">
+    <script src='http://code.jquery.com/jquery-1.11.0.min.js'></script>
+</head> 
+
+<body>
+    <div id="menu">
+        <div id="logo">
+            <a href="index.php">
+                <img src="photos/logosmallblue.png" alt="Michelle Nelson Photography">
+            </a>
+        </div>
+        <div class="seprub"></div>
+        <ul class="a">
+            <li><a href="pages/portfolio.php">portfolio</a></li>
+            <li><a href="pages/about.php">about</a></li>
+            <li><a href="pages/contact.php">contact</a></li>
+        </ul>
+        <div class="seprub"></div>
+        <ul class="social">
+                <li><a href="http://instagram.com/whimsicolour" target="_blank">
+                    <img src="photos/socialmedia/insta.png" alt="Instagram">
+                </a></li>
+                <li><a href="https://www.behance.net/michellenelson" target="_blank">
+                    <img src="photos/socialmedia/be.png" alt="Behance">
+                </a></li>
+                <li><a href="https://www.facebook.com/michellehnelson" target="_blank">
+                    <img src="photos/socialmedia/fb.png" alt="Facebook">
+                </a></li>
+        </ul>
+    </div>
+    
+    <div id="mainhome">
+        <ul class="b">
+            <li><a href="pages/nature.php">
+               <img src="photos/homepage_icons/nature.jpg" alt="Nature">
+                <span class="text-content"><span>nature</span></span>
+             </a></li>
+            <li><a href="pages/scenic.php">
+                <img src="photos/homepage_icons/scenic.jpg" alt="Scenic">
+                <span class="text-content"><span>landscape</span></span>
+            </a></li>
+            <li><a href="pages/film.php">
+                <img src="photos/homepage_icons/film.jpg" alt="Film">
+                <span class="text-content"><span>film</span></span>
+            </a></li>
+            <li><a href="pages/bw.php">
+                <img src="photos/homepage_icons/bw.jpg" alt="Black & White">
+                <span class="text-content"><span>black & white</span></span>
+            </a></li>
+            <li><a href="pages/macro.php">
+                <img src="photos/homepage_icons/macro.jpg" alt="Macro">
+                <span class="text-content"><span>macro</span></span>
+            </a></li>
+            
+            <li><a href="pages/nature.php">
+               <img src="photos/homepage_icons/nature2.jpg" alt="Nature">
+                <span class="text-content"><span>nature</span></span>
+             </a></li>
+            <li><a href="pages/scenic.php">
+                <img src="photos/homepage_icons/scenic2.jpg" alt="Scenic">
+                <span class="text-content"><span>landscape</span></span>
+            </a></li>
+            <li><a href="pages/film.php">
+                <img src="photos/homepage_icons/film2.jpg" alt="Film">
+                <span class="text-content"><span>film</span></span>
+            </a></li>
+            <li><a href="pages/nature.php">
+               <img src="photos/homepage_icons/nature3.jpg" alt="Nature">
+                <span class="text-content"><span>nature</span></span>
+             </a></li>
+            <li><a href="pages/macro.php">
+                <img src="photos/homepage_icons/macro2.jpg" alt="Macro">
+                <span class="text-content"><span>macro</span></span>
+            </a></li>
+            <li><a href="pages/scenic.php">
+                <img src="photos/homepage_icons/scenic3.jpg" alt="Scenic">
+                <span class="text-content"><span>landscape</span></span>
+            </a></li>
+            <li><a href="pages/nature.php">
+               <img src="photos/homepage_icons/nature4.jpg" alt="Nature">
+                <span class="text-content"><span>nature</span></span>
+             </a></li>
+        </ul>
+    </div>
+
+    <script src='js/script.js'></script>
+    
+</body>
+</html>
